5d111a5d4a2fbfdadc218c678b81255d0b531ae7
Matteo <matteo.grz2006@yahoo.com>
2021-04-04 16:18:18 +0200

    First check.

--- js/index.js
+++ js/index.js
@@ -57,3 +57,73 @@ function change(value) {
         alfabetoList.style.height = "0"        
     }
 }
+
+function getDirections(startPole, place) {
+
+    let directionsText = document.getElementById('directions-text');
+    let directionsArray = [];
+    let listOfDirections = [];
+    let list = document.getElementById("list");
+
+    if (directionsText.childElementCount = 1) {
+        directionsText.innerHTML = ""
+    }
+
+    let heading = document.createElement('h2');
+    heading.id = 'directions-text'
+
+    if (startPole == 1) {
+        if (place == 'myp') {
+            heading.innerHTML = "MYP & DP";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+        else if (place == 'pyp') {
+            heading.innerHTML = "PYP";
+            directionsArray = ['⬇️ for 10m', '⬅️'];
+        }
+        else if (place == 'college') {
+            heading.innerHTML = "College";
+            directionsArray = ['⬆️ for 250m', '➡️'];
+        }
+        else if (place == 'student-house') {
+            heading.innerHTML = "Student House";
+            directionsArray = ['⬆️ for 50m', '⬅️', '⬆️ for 20m', '⬅️', '⬆️ for 50m', '➡️'];
+        }
+        else if (place == 'library') {
+            heading.innerHTML = "Library";
+            directionsArray = ['⬆️ for 50m', '⬅️ for 10m', '⬆️'];
+        }
+        else if (place == 'sports-center') {
+            heading.innerHTML = "Sports Center";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+        else if (place == 'bistro') {
+            heading.innerHTML = "Bistro";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+        else if (place == 'football') {
+            heading.innerHTML = "Football";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+        else if (place == 'hall') {
+            heading.innerHTML = "Hall";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+        else if (place == 'sierra') {
+            heading.innerHTML = "Sierra";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+        else if (place == 'innovation-area') {
+            heading.innerHTML = "Innovation Area";
+            directionsArray = ['⬆️ for 100m', '➡️'];
+        }
+
+        directionsArray.forEach(function(element) {
+            listOfDirections.push("<li>" + element + "</li>");
+        });
+          
+        list.innerHTML = listOfDirections.join('');
+    }
+
+    directionsText.appendChild(heading, list)
+}
